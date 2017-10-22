77848d46f6aa025edb1e57116819aed6de525562
Joan Vasquez <yoloprogramo22@gmail.com>
2017-10-22 13:51:59 -0400

    Updating UserTest class

--- src/test/java/user/managment/desktop/test/UserTest.java
+++ src/test/java/user/managment/desktop/test/UserTest.java
@@ -3,6 +3,9 @@ package user.managment.desktop.test;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -13,77 +16,68 @@ import user.managment.desktop.db.DBConnection;
 import user.managment.desktop.db.DBConnectionProperties;
 import user.managment.desktop.model.User;
 import user.managment.desktop.security.AES256;
+import user.managment.desktop.views.Desktop;
 
 //USER TEST CLASS
 public class UserTest {
 
 	private static UserController userController;// USER DAO
 	private static User user;// USER ENTITY
-
-	private DBConnection dbConnection;// DATABASE CONNECTION
-	private PreparedStatement prepareStatement;
-	private ResultSet resultSet;
-	private DBConnectionProperties dbConnectionProperties;
-	private int idUser = 0;// USER ID
-
+	private static DBConnection dbConnection;// DATABASE CONNECTION
+	private static DBConnectionProperties dbConnectionProperties;
+	
 	@BeforeClass
 	public static void setUpClass() {
-		user = new User();
-	}
-
-	@Test // SAVE USER TEST
-	public void test1() throws SQLException {
-		userController = new UserController();
-		user.setEmail("yoloprogramo22@gmail.com");
-		user.setFirstName("Test insert");
-		user.setLastName("Test insert");
-		user.setPassword(AES256.encryption("pass"));
-		Assert.assertNotEquals(null, userController.saveUser(user));
-	}
-
-	@Before // GET AN USER ID
-	public void test2() throws SQLException {
+		Desktop.isTest = true;
 		dbConnectionProperties = new DBConnectionProperties();
-		dbConnectionProperties.setDriver("com.mysql.jdbc.Driver");
-		dbConnectionProperties.setUrl("jdbc:mysql://localhost:3306/userprocesses");
-		dbConnectionProperties.setUser("root");
-		dbConnectionProperties.setPassword("pass22");
+		dbConnectionProperties.setDriver("com.mysql.jdbc.Driver");// THE DRIVER - MYSQL IN THIS CASE
+		dbConnectionProperties.setUrl("jdbc:mysql://localhost:3306/userprocesses_test");// DATABASE URL
+		dbConnectionProperties.setUser("root");// DATABASE USER
+		dbConnectionProperties.setPassword("pass");// DATABASE PASSWORD
 		dbConnection = DBConnection.openConnection(dbConnectionProperties);
-
-		prepareStatement = dbConnection.getConnection().prepareStatement("SELECT iduser FROM user LIMIT 1");
-		resultSet = prepareStatement.executeQuery();
-		if (resultSet.next())
-			idUser = resultSet.getInt(1);
-
-		resultSet.close();
-		prepareStatement.close();
-		dbConnection.closeConnection();
-	}
-
-	@Test // UPDATE USER TEST
-	public void test3() throws SQLException {
-		if (idUser > 0) {
-			userController = new UserController();
-			user.setIdUser(idUser);
-			user.setFirstName("Test update");
-			user.setLastName("Test update");
-			user.setPassword(AES256.encryption("pass"));
-			Assert.assertTrue(userController.updateUser(user));
+		try {
+			dbConnection.getConnection().setAutoCommit(false);
+		}catch(Exception e) {
+			System.err.println("Error :: " + e.getMessage());
 		}
-	}
-
-	@Test // GET ALL OF THE USER TEST
-	public void test4() throws SQLException {
+		user = new User();
 		userController = new UserController();
-		Assert.assertNotEquals(0, userController.readUser());
 	}
-
-	@Test // DELETE USER TEST
-	public void test7() throws SQLException {
-		if (idUser > 0) {
-			userController = new UserController();
-			Assert.assertTrue(userController.deleteUser(idUser));
+	
+	@Test
+	public void testUserController() {
+		user.setFirstName("Test");
+		user.setLastName("Test");
+		user.setEmail("test@test.com");
+		user.setPassword(AES256.encryption("123"));
+		
+		try {
+			user = userController.saveUser(user);
+			Assert.assertNotEquals(0, user.getIdUser());
+			user.setFirstName("update test");
+			boolean result = userController.updateUser(user);
+			Assert.assertTrue(result);
+			
+			List<User> users = userController.readUser();
+			Assert.assertNotEquals(0, users.size());
+			
+			result = userController.deleteUser(user.getIdUser());
+			Assert.assertTrue(result);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-
+	}
+	
+	@AfterClass
+	public static void endTest() {
+		try {
+			dbConnection.getConnection().rollback();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			DBConnection.closeConnection();
+		}
 	}
 }
